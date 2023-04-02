a533dabcb0bed2bd7d186758c5a6d4ad8159af20
gibbssonfarias30 <gibbfarc17@gmail.com>
2023-04-02 16:19:32 -0500

    utils package removed

--- src/main/java/com/backfcdev/personal_task_management/utils/Mapper.java
+++
@@ -1,9 +0,0 @@
-package com.backfcdev.personal_task_management.utils;
-
-import org.modelmapper.ModelMapper;
-
-public class Mapper {
-    public static ModelMapper modelMapper(){
-        return new ModelMapper();
-    }
-}
