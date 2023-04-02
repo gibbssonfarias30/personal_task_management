12bfe7c1048341646570ecba76f3336256c50210
gibbssonfarias30 <gibbfarc17@gmail.com>
2023-04-02 16:07:59 -0500

    update service

--- src/main/java/com/backfcdev/personal_task_management/service/ITaskService.java
+++ src/main/java/com/backfcdev/personal_task_management/service/ITaskService.java
@@ -1,17 +1,16 @@
 package com.backfcdev.personal_task_management.service;
 
 import com.backfcdev.personal_task_management.model.Task;
-import com.backfcdev.personal_task_management.model.dto.TaskDTO;
 
 import java.util.List;
 
 public interface ITaskService {
 
-    List<Task> getAll();
-    Task save(TaskDTO taskDTO);
-    Task getById(long id);
-    Task update(long id, TaskDTO taskDTO);
+    List<Task> findAll();
+    Task save(Task task);
+    Task findById(long id);
+    Task update(long id, Task task);
     List<Task> getAllTasksAccordingToStatus(String status);
     Task markTaskAccomplished(long id);
-    long deleteById(long id);
+    void delete(long id);
 }
